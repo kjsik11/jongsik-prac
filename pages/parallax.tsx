53afeed0046cd4f5af3a5e9a75891802c65bde56
jongsik <kjsik11@gmail.com>
2021-10-04 22:07:11 +0900

    minor fix

--- pages/parallax.tsx
+++ pages/parallax.tsx
@@ -11,7 +11,7 @@ export default function ParallaxPage() {
   const [inViewRef5, inView5] = useInView({ rootMargin: '300px' });
 
   const [{ x }, api] = useSpring(() => ({
-    x: 100,
+    x: 0,
     config: { duration: 300 },
   }));
 
@@ -77,11 +77,29 @@ export default function ParallaxPage() {
 
   return (
     <div>
-      <div className="h-[1200px]" />
-
+      <div className="h-[1000px] flex justify-center items-center max-w-3xl mx-auto">
+        <p>
+          What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,
+          when an unknown printer took a galley of type and scrambled it to make a type specimen
+          book. It has survived not only five centuries, but also the leap into electronic
+          typesetting, remaining essentially unchanged. It was popularised in the 1960s with the
+          release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+          publishing software like Aldus PageMaker including versions of Lorem Ipsum. Why do we use
+          it? It is a long established fact that a reader will be distracted by the readable content
+          of a page when looking at its layout. The point of using Lorem Ipsum is that it has a
+          more-or-less normal distribution of letters, as opposed to using Content here, content
+          here, making it look like readable English. Many desktop publishing packages and web page
+          editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will
+          uncover many web sites still in their infancy. Various versions have evolved over the
+          years, sometimes by accident, sometimes on purpose (injected humour and the like).
+        </p>
+      </div>
       <animated.div
         style={{ left: x.to((x) => `${x}%`) }}
-        className={'mx-60 w-60 h-40 bg-red-400 rounded-md sticky top-40'}
+        className={
+          'mx-60 w-60 h-40 bg-red-400 rounded-md sticky top-40 flex items-center justify-center'
+        }
       >
         <p className="text-center text-2xl">a sticky layer</p>
       </animated.div>
