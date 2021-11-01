2226d505df919115b8b3b0b41c3c7426c7aa403c
jongsik <kjsik11@gmail.com>
2021-11-01 14:33:36 +0900

    add check before install prompt code

--- components/layout/Common.tsx
+++ components/layout/Common.tsx
@@ -1,10 +1,34 @@
+import { useEffect } from 'react';
+
 import { Modal, Notification } from '@components/ui';
 
 import { useUI } from '../context';
 
-
 export default function CommonLayout({ children }: { children: React.ReactNode }) {
-  const { modalFlag, modalContent, notiFlag, closeNoti, notiContent } = useUI();
+  const { showModal, closeModal, modalFlag, modalContent, notiFlag, closeNoti, notiContent } =
+    useUI();
+
+  useEffect(() => {
+    // Initialize deferredPrompt for use later to show browser install prompt.
+    let deferredPrompt;
+
+    window.addEventListener('beforeinstallprompt', (e) => {
+      console.log('hello');
+      // Prevent the mini-infobar from appearing on mobile
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+      // Update UI notify the user they can install the PWA
+      showModal({
+        title: 'install?',
+        content: 'real',
+        actionButton: { onClick: () => {}, label: 'yes' },
+        cancelButton: { onClick: () => closeModal(), label: 'no' },
+      });
+      // Optionally, send analytics event that PWA install promo was shown.
+      console.log(`'beforeinstallprompt' event was fired.`);
+    });
+  }, []);
 
   return (
     <div className="relative h-full w-full">
