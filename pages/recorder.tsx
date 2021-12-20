41e367d2158e2a97cea0ec625a3c1749d9b14b88
jongsik <kjsik11@gmail.com>
2021-12-20 20:14:45 +0900

    minor css fix

--- pages/recorder.tsx
+++ pages/recorder.tsx
@@ -1,21 +1,21 @@
+import { Button } from '@components/ui';
+
 import useRecorder from '@lib/hooks/use-recoder';
 
 export default function RecorderPage() {
   const { audioURL, isRecording, startRecording, stopRecording } = useRecorder();
 
   return (
-    <div className="App">
+    <div className="mx-auto max-w-2xl mt-20">
       <audio src={audioURL} controls />
-      <button onClick={startRecording} disabled={isRecording}>
-        start recording
-      </button>
-      <button onClick={stopRecording} disabled={!isRecording}>
-        stop recording
-      </button>
-
-      <p>
-        <em>(On Codesandbox pop out the preview into a window to get a user media request.)</em>
-      </p>
+      <div className="space-x-2 my-4">
+        <Button onClick={startRecording} disabled={isRecording}>
+          start recording
+        </Button>
+        <Button onClick={stopRecording} disabled={!isRecording}>
+          stop recording
+        </Button>
+      </div>
     </div>
   );
 }
